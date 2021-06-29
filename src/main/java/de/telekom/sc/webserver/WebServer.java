4748b6da897b2c80d41da338585fa6e17fc9b1bb
juliShmi <juliashmitko@gmail.com>
2021-06-29 09:00:43 +0300

    Draft 80 percent ready

---
+++ src/main/java/de/telekom/sc/webserver/WebServer.java
@@ -0,0 +1,12 @@
+package de.telekom.sc.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServer /* implements SpringBootApplication */ {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServer.class, args);
+	}
+}
